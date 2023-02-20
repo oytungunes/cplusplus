032e460b5a9add95780f49391c4d21fdd2e0ad7c
Oytun Gunes <71605451+oytungunes@users.noreply.github.com>
2023-02-20 11:10:13 +0300

    Update forloop.cpp

--- forloop.cpp
+++ forloop.cpp
@@ -38,4 +38,20 @@ int main() {
   return 0;
 }
 
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i = 0;
+  while (i < 10) {
+    cout << i << "\n";
+    i++;
+    if (i == 4) {
+      break;
+    }
+  } 
+  return 0;
+}
+
+
 
