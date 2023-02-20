9afccca499e5a86d11a4a52b532dab4a0ce11f59
Oytun Gunes <71605451+oytungunes@users.noreply.github.com>
2023-02-20 10:14:40 +0300

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,25 @@
+//int - stores integers (whole numbers), without decimals, such as 123 or -123
+//double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+//char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+//string - stores text, such as "Hello World". String values are surrounded by double quotes
+// bool - stores values with two states: true or false
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNum = 15;
+  cout << myNum;
+  return 0;
+}
+
+// Note that if you assign a new value to an existing variable, it will overwrite the previous value:
+
+
+int myNum = 15;  // myNum is 15
+myNum = 10;  // Now myNum is 10
+cout << myNum;  // Outputs 10
+
+
