473c7da2c35455b4bd3007a51b4a817e661dbd9e
Oytun Gunes <71605451+oytungunes@users.noreply.github.com>
2023-02-20 10:22:28 +0300

    Update variables.cpp

--- variables.cpp
+++ variables.cpp
@@ -31,3 +31,14 @@ cout << x + y + z;
 int x, y, z;
 x = y = z = 50;
 cout << x + y + z;
+
+
+//Constants
+//When you do not want others (or yourself) to change existing variable values, use the const keyword (this will declare the variable as "constant", which means unchangeable and read-only):
+const int myNum = 15;  // myNum will always be 15
+myNum = 10;  // error: assignment of read-only variable 'myNum'
+
+
+const int minutesPerHour = 60;
+const float PI = 3.14;
+
