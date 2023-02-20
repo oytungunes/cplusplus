ebcfedc4a2fb3c813591575afcdcf6f5fdbbabe5
Oytun Gunes <71605451+oytungunes@users.noreply.github.com>
2023-02-20 10:15:36 +0300

    Update variables.cpp

--- variables.cpp
+++ variables.cpp
@@ -23,3 +23,11 @@ myNum = 10;  // Now myNum is 10
 cout << myNum;  // Outputs 10
 
 
+//Declare multiple variables
+
+int x = 5, y = 6, z = 50;
+cout << x + y + z;
+
+int x, y, z;
+x = y = z = 50;
+cout << x + y + z;
