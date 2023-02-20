d273e95fa349c51782c627f5af43a2d5ddaad12f
Oytun Gunes <71605451+oytungunes@users.noreply.github.com>
2023-02-20 11:00:27 +0300

    Create while_dowhile.cpp

---
+++ while_dowhile.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i = 0;
+  while (i < 5) {
+    cout << i << "\n";
+    i++;
+  }
+  return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i = 0;
+  do {
+    cout << i << "\n";
+    i++;
+  }
+  while (i < 5);
+  return 0;
+}
+
