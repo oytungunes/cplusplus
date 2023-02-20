5557ffbd3fabf6c8f3653ab5033fc7ba0a9f0b79
Oytun Gunes <71605451+oytungunes@users.noreply.github.com>
2023-02-20 10:38:41 +0300

    Create datatypesandoperators.cpp

---
+++ datatypesandoperators.cpp
@@ -0,0 +1,41 @@
+
+Data Type	Size	Description
+boolean	1 byte	Stores true or false values
+char	1 byte	Stores a single character/letter/number, or ASCII values
+int	  2 or 4 bytes	Stores whole numbers, without decimals
+float 	4 bytes	Stores fractional numbers, containing one or more decimals. Sufficient for storing 6-7 decimal digits
+double	8 bytes	Stores fractional numbers, containing one or more decimals. Sufficient for storing 15 decimal digits
+
+
+int myNum = 5;               // Integer (whole number)
+float myFloatNum = 5.99;     // Floating point number
+double myDoubleNum = 9.98;   // Floating point number
+char myLetter = 'D';         // Character
+bool myBoolean = true;       // Boolean
+string myText = "Hello";     // String
+float f1 = 35e3;
+double d1 = 12E4;
+
++	Addition	Adds together two values	x + y	
+-	Subtraction	Subtracts one value from another	x - y	
+*	Multiplication	Multiplies two values	x * y	
+/	Division	Divides one value by another	x / y	
+%	Modulus	Returns the division remainder	x % y	
+++	Increment	Increases the value of a variable by 1	++x	
+--	Decrement	Decreases the value of a variable by 1	--x
+
+
+Operators
+
+Operator	Name	Example	Try it
+==	Equal to	x == y	
+!=	Not equal	x != y	
+>	Greater than	x > y	
+<	Less than	x < y	
+>=	Greater than or equal to	x >= y	
+<=	Less than or equal to	x <= y
+
+&& 	Logical and	Returns true if both statements are true	x < 5 &&  x < 10	
+|| 	Logical or	Returns true if one of the statements is true	x < 5 || x < 4	
+!	Logical not	Reverse the result, returns false if the result is true	!(x < 5 && x < 10)
+
