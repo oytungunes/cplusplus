a9d6a767f1ec3b90af9fe6c1e505ddf333938d9e
Oytun Gunes <71605451+oytungunes@users.noreply.github.com>
2023-02-21 15:16:25 +0300

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -36,3 +36,20 @@ int main() {
   }
   return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  string letters[2][4] = {
+    { "A", "B", "C", "D" },
+    { "E", "F", "G", "H" }
+  };
+  
+  cout << letters[0][2];
+  return 0;
+}
+
+
+
+
